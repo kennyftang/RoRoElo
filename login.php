79d660facfc1c3ecca7221978432bb31e84bc197
Kenny <kennyftang@gmail.com>
2017-01-26 23:04:13 -0600

    Logging in and registering works

--- login.php
+++ login.php
@@ -14,38 +14,50 @@
 
 function printLoginForm()
 {
-	echo("<form method='post'>\r\n");
-	echo("\t<label>Username</label></br><input name='username' type='text' id='username'></br>\r\n");
-	echo("\t<label>Password</label></br><input name='password' type='password' id='password'></br>\r\n");
-	echo("\t<button type='submit'>Submit</button></br>\r\n");
-	echo("</form>\r\n");
+	echo("<form method='post'>
+    <label>Username</label></br><input name='username' type='text' id='username'></br>
+    <label>Password</label></br><input name='password' type='password' id='password'></br>
+    <button type='submit'>Submit</button></br>
+</form>");
 }
-if(isset($_POST['username']))
-	$username = $_POST['username'];
+if(isset($_POST["username"]))
+	$username = $_POST["username"];
 else {
 	printLoginForm();
-	die("username");
+	die();
 }
-if(isset($_POST['password']))
-	$password = $_POST['password'];
+if(isset($_POST["password"]))
+	$password = $_POST["password"];
 else {
 	printLoginForm();
-	die("password");
+	die();
 }
-echo 'did not die';
 $dsn = "sqlite:".__DIR__."/db/rrtt.sqlite";
 $pdo = new PDO($dsn);
-if(preg_match("/\$RRHStabletennis107:.*/", $username)) {
-    $registerstmt = $pdo->prepare("INSERT INTO logins VALUES(id,username,password)");
+if(preg_match("/^RRHStabletennis107:.*/", $username)) {
+    password_hash($password, PASSWORD_DEFAULT);
+    $checkstmt = $pdo->prepare("SELECT COUNT(*) FROM `logins` WHERE `username` = :username");
+    $checkstmt->execute(array(":username" => $username));
+    $check = $checkstmt->fetch()[0];
+    $username = explode(":", $username, 2)[1];
+    if($check == 0) {
+        $pdo->beginTransaction();
+        $registerstmt = $pdo->prepare("INSERT INTO logins (username,password) VALUES(?, ?)");
+        $hashed = password_hash($password, PASSWORD_DEFAULT);
+        $registerstmt->execute(array($username, $hashed));
+        echo("<p>User $username has been added!</p>\r\n");
+        $pdo->commit();
+    }
 }
-$loginstmt = $pdo->prepare("SELECT * FROM logins WHERE username = :username");
-$loginstmt->execute(array(':username' => $username));
+$loginstmt = $pdo->prepare("SELECT * FROM `logins` WHERE `username` = ?");
+$loginstmt->execute(array($username));
 $login = $loginstmt->fetch();
-if(password_verify($password, $login['password'])) {
-
+if(password_verify($password, $login["password"])) {
+    echo("
+    ");
 }
 else
-	die("\t<p>Username or Password is incorrect</p>\r\n");
+	echo("<p>Username or Password is incorrect</p>\r\n");
 ?>
 </body>
 </html>
